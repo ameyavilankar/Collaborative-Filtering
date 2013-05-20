708a61e133736e12dadcf5a8acd7e6c36326a58a
Ameya Vilankar <ameya.vilankar@gmail.com>
2013-05-20 17:52:49 -0400

    Made changes to getUserCluster()

--- getUsersClusters.cpp
+++ getUsersClusters.cpp
@@ -15,6 +15,7 @@ public:
     inline long getMovieId() const { return movieId; }
     inline void setMovieId(long id) { movieId = id; }
 
+    Movie(long id):movie_id(id) {}
 private:
     long movieId;                                           // Used to uniquely identify a Movie
     
@@ -67,6 +68,7 @@ public:
     }
 
     // TODO: Add Movie by id
+    User(long id): userId(id) {}
 
 private:
     long userId;                                            // Stores the id of the User.
@@ -96,7 +98,17 @@ public:
     //inline const std::map<long, User> getUsersFromCluster() const { return users; }
 
     //inline const std::map<long, Movie> geMovieUniverse() const { return movieUniverse; }
-
+    
+    // Cluster Constructor
+    Cluster(int id): clusterId(id) {}
+    
+    // Add user to the cluster
+    void addUser(const User& user)
+    {
+        // add the user to the map of users
+        users[user.getUserId()] = users;
+    }
+    
 private:
     int clusterId;                                          // Cluster Id to identify the cluster.
     std::vector<double> clusterCenter;                      // Store the center of the cluster for Difference Calculations.
@@ -104,48 +116,51 @@ private:
     std::map<long, Movie> movieUniverse;                          // Universe of all the movies rated by users in the cluster
 };
 
+// Used to Compare two Clusters by their Ids
+bool operator<(const Cluster& one, const Cluster& two)
+{
+    return (one.getClusterId() < two.getClusterId());
+}
 
 int main()
 { 
     // Replace by unordered_map or unordered_set
-    std::map<int, std::vector<long> > clusterMap;
+    std::map<int, Cluster> clusterMap;
 
-    // Open the file for getting the input
-    ifstream myfile("data_with_id.txt");
+    {
+        // Open the file for getting the input
+        ifstream myfile("data_with_id.txt");
+        
+        //Always test the file open.
+        if(!myfile) 
+        {
+          cout<<"Error opening output file"<<endl;
+          system("pause");
+          return -1;
+        }
+
+        // Temporary Variables to read the Data
+        long userNo;
+        int clusterNo;
+
+        // Till there is no output or u get an error, add the user to its correpsonding cluster.
+        while(myfile>>userNo>>clusterNo)
+        {
+            clusterMap[clusterNo] = Cluster(clusterNo);
+            cluserMap[clusterNo].addUser(User(userNo));
+        }
+         
+
+        // Clear and close the ifstream
+        myfile.clear();
+        myfile.close();
+    }
     
-    //Always test the file open.
-    if(!myfile) 
-    {
-      cout<<"Error opening output file"<<endl;
-      system("pause");
-      return -1;
-    }
-
-    // Temporary Variables to read the Data
-    long user;
-    int clusterNo;
-
-    // Till there is no output or u get an error, add the user to its correpsonding cluster.
-    while(myfile>>user>>clusterNo)
-      clusterMap[clusterNo].push_back(user);
-
-    cout<<"Clusters and their correpsonding Users: "<<endl;
-    for(std::map<int, std::vector<long> >::const_iterator it = clusterMap.begin(); it != clusterMap.end(); it++)
-    {
-        cout<<"Cluster No.: "<<it->first<<endl;
-
-        // Output all the users in that cluster
-        for(int i = 0; i < it->second.size(); i++)
-            cout<<it->second[i]<<" ";
-
-        cout<<endl;
-    }
-
     // OPTIONAL STEP: For each Cluster sort the users according to their ids
     for(std::map<int, std::vector<long> >::iterator it = clusterMap.begin(); it != clusterMap.end(); it++)
         sort(it->second.begin(), it->second.end());
 
-    cout<<"Clusters and their correpsonding Users SORTED according to the userid: "<<endl;
+    cout<<"Clusters and their corresponding Users SORTED according to the userid: "<<endl;
     for(std::map<int, std::vector<long> >::const_iterator it = clusterMap.begin(); it != clusterMap.end(); it++)
     {
         cout<<"Cluster No.: "<<it->first<<endl;
@@ -157,5 +172,63 @@ int main()
         cout<<endl;
     }
 
+    // Create a map of map to store the user ratings.
+    map<long, map<long, double> > ratingMatrix;
+    
+    {
+        // Open the ratings file to read in the user ratings for the movies.
+        ifstream myfile("ratings_with_id.txt");
+
+        if(!myfile) 
+        {
+          cout<<"Error opening output file"<<endl;
+          system("pause");
+          return -1;
+        }
+
+        // To hold the entire currentline
+        std::string currentLine;
+
+        // To hold the double values from the currentline
+        std::vector<double> splitDouble;
+        
+        while (std::getline (myfile, currentLine))
+        {
+            //cout<<count<<endl;
+            // Split the currentLine and only return the double parts
+            splitDouble = split(currentLine);
+            
+            // Store the rating in the map
+            ratingMatrix[splitDouble[0]][splitDouble[1]] = splitDouble[2];
+        }
+
+        cout<<"Number of Users: "<<ratingMatrix.size();
+
+        for(int i = 0; i < ratingMatrix.size(); i++)
+        {
+            cout<<i<<" ";
+            for(int j = 0; j < ratingMatrix[i].size(); j++)
+                cout<<ratingMatrix[i][j]<<" ";
+            cout<<endl;
+        }
+
+        // Clear and close the ifstream
+        myfile.clear();
+        myfile.close();
+    }
+
+    //Create the universe of all movies
+    map<long, Movie> movieUniverse;
+
+    // Create a map of all users
+    map<long, User> allUsers;
+
+    // Create a map of Clusters
+    map<int, Cluster> allClusters;
+
+    for(map<long, map<long, double> >::const_iterator it = ratingMatrix.begin(); it != ratingMatrix.end(); it++)
+    {
+
+    }
     return 0;
 }
