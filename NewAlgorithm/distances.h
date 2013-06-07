c9bebbe3fbf1b0e05a081fba54bf8e3beec1df6e
Ameya Vilankar <ameya.vilankar@gmail.com>
2013-06-07 18:45:04 +0000

    Edited the distances.h. Proeperly Indented, added comments, added some similarity measures to be used by memory based cf algorithms.

--- NewAlgorithm/distances.h
+++ NewAlgorithm/distances.h
@@ -30,15 +30,71 @@ double findCommon(const std::vector<double>& one, const std::vector<double>& two
 }
 */
 
-double pearsonCoefficient(const std::vector<double>&, const std::vector<double>&);
-double euclidean(const std::vector<double>&, const std::vector<double>&);
+// used to calculate the euclidean distance between two vectors
+double euclidean(const std::vector<double>& one, const std::vector<double>& two)
+{
+	assert(one.size() == two.size());
+	
+	double sum = 0.0;
+	for(int i = 0; i < one.size(); i++)
+		sum += (one[i] - two[i]) * (one[i] - two[i]);
+	
+	return sum;
+}
+
+// used to calculate the mean of the vector
+double mean(const std::vector<double> one)
+{
+	assert(one.size() >= 0);
+	
+	double mean = 0.0;
+	
+	for(int i = 0 ; i < one.size(); i++)
+		mean += one[i];
+	
+	return mean/one.size();
+}
+
+// calculates the pearson coefficient between two vectors
+double pearsonCoefficient(const std::vector<double>& one, const std::vector<double>& two)
+{
+	assert(one.size() == two.size());
+	// Calculate the mean of the two vectors
+	double meanOne = mean(one);
+	double meanTwo = mean(two);
+	
+	// Calculate the square of difference and the product of the differences
+	double oneSqrDiff = 0.0;
+	double twoSqrDiff = 0.0;
+	double prodDiff = 0.0;
+	double oneDiff = 0.0;
+	double twoDiff = 0.0;
+	
+	for(int i = 0; i < one.size(); i++)
+	{
+		oneDiff = (one[i] - meanOne);
+		twoDiff = (two[i] - meanTwo);
+		
+		prodDiff += oneDiff * twoDiff;
+		oneSqrDiff += oneDiff * oneDiff;
+		twoSqrDiff += twoDiff * twoDiff;
+	
+	}
+	
+	double denominator = sqrt(oneSqrDiff * twoSqrDiff);
+	
+	if(denominator == 0)
+		return 0;
+	
+	return prodDiff/denominator;
+}
 
 double calcPearson(const std::vector<double>& one, const std::vector<double>& two)
 {
 	std::vector<double> oneCommon;
 	std::vector<double> twoCommon;
 	
-	for(int i = 0; i < one.size(); i++)
+	for(int i = 0; i < one.size();i++)
 		if(one[i] != 0 && two[i] != 0)
 		{
 			oneCommon.push_back(one[i]);
@@ -67,181 +123,114 @@ double calcEuclidean(const std::vector<double>& one, const std::vector<double>& 
 	return euclidean(oneCommon, twoCommon);
 }
 
-
-// used to calculate the euclidean distance between two vectors
-double euclidean(const std::vector<double>& one, const std::vector<double>& two)
+# Calculate the similarity measure
+double calcEuclideanSimilarity(const std::vector<double>& one, const std::vector<double>& two)
 {
-  assert(one.size() == two.size());
-
-  double sum = 0.0;
-  for(int i = 0; i < one.size(); i++)
-    sum += (one[i] - two[i]) * (one[i] - two[i]);
-
-  return sum;
-}
-
-// used to calculate the mean of the vector
-double mean(const std::vector<double> one)
-{
-  assert(one.size() >= 0);
-
-  double mean = 0.0;
-  for(int i = 0 ; i < one.size(); i++)
-    mean += one[i];
-
-  return mean/one.size();
-}
-
-// calculates the pearson coefficient between two vectors
-double pearsonCoefficient(const std::vector<double>& one, const std::vector<double>& two)
-{
-  assert(one.size() == two.size());
-
-  // Calculate the mean of the two vectors
-  double meanOne = mean(one);
-  double meanTwo = mean(two);
-
-  // Calculate the square of difference and the product of the differences
-  double oneSqrDiff = 0.0;
-  double twoSqrDiff = 0.0;
-  double prodDiff = 0.0;
-  double oneDiff = 0.0;
-  double twoDiff = 0.0;
-
-  for(int i = 0; i < one.size(); i++)
-  {
-    oneDiff = (one[i] - meanOne);
-    twoDiff = (two[i] - meanTwo);
-
-    prodDiff += oneDiff * twoDiff;
-    oneSqrDiff += oneDiff * oneDiff;
-    twoSqrDiff += twoDiff * twoDiff;
-  }
-  
-  double denominator = sqrt(oneSqrDiff * twoSqrDiff);
-	
-  /*
-  std::cout<<"Pearson Coefficient: "<<prodDiff/denominator<<std::endl;
-  std::cout<<"oneSqrDiff: "<<oneSqrDiff<<" twoSqrDIff "<<twoSqrDiff<<std::endl;
-  std::cout<<"OneCommon: ";
-  
-  for(int i = 0; i < one.size(); i++)
-	  std::cout<<one[i]<<" ";
-  std::cout<<std::endl;
-  
-  for(int i = 0; i < two.size(); i++)
-	  std::cout<<two[i]<<" ";
-  std::cout<<std::endl;
-  */
-
-  if(denominator == 0)
-	  return 0;
-
-  return prodDiff/denominator;
+	return (1.0/(1.0 + calcEuclidean(one, two)));
 }
 
 // calculates the cosine similarity between two vectors
 double consineDistance(const std::vector<double>& one, const std::vector<double>& two)
 {
-  assert(one.size() == two.size());
-
-  double oneSqrSum = 0.0;
-  double twoSqrSum = 0.0;
-  double prodSum = 0.0;
-  for(int i = 0; i < one.size(); i++)
-  {
-    prodSum += (one[i] * two[i]);
-    oneSqrSum += one[i] * one[i];
-    twoSqrSum += two[i] * two[i];
-  }
-
-  double denominator = sqrt(oneSqrSum * twoSqrSum);
-  
-  if(denominator == 0)
-    return 1.0;
-  else
-    return 1.0 - prodSum/denominator;
+	assert(one.size() == two.size())
+	
+	double oneSqrSum = 0.0;
+	double twoSqrSum = 0.0;
+	double prodSum = 0.0;
+	
+	for(int i = 0; i < one.size(); i++)
+	{
+		prodSum += (one[i] * two[i]);
+		oneSqrSum += one[i] * one[i];
+		twoSqrSum += two[i] * two[i];
+  	}
+	
+	double denominator = sqrt(oneSqrSum * twoSqrSum);
+	
+	if(denominator == 0)
+		return 1.0;
+	else
+		return 1.0 - prodSum/denominator;
 }
 
 // calculates the jaccardDistance between the two vectors
 double jaccardDistance(const std::vector<double>& one, const std::vector<double>& two)
 {
-  assert(one.size() == two.size());
-
-  int unionCount = 0;
-  int intersectionCount = 0;
-
-  for(int i = 0 ; i < one.size(); i++)
-  {
-    if(one[i] != 0 && two[i] != 0)
-      intersectionCount++;
-
-    if(one[i] != 0 || two[i] != 0)
-      unionCount++;
-  }
-
-  if(unionCount == 0);
-    return 1.0;
-
-  return 1.0 - (double)intersectionCount/unionCount;
+	assert(one.size() == two.size());
+	int unionCount = 0;
+	int intersectionCount = 0;
+	
+	for(int i = 0 ; i < one.size(); i++)
+	{
+		if(one[i] != 0 && two[i] != 0)
+			intersectionCount++;
+		
+		if(one[i] != 0 || two[i] != 0)
+			unionCount++;
+  	}
+	
+	if(unionCount == 0)
+		return 1.0;
+		
+	return 1.0 - (double)intersectionCount/unionCount;
 }
 
 // calculates the tanimoto distance between two vectors 
 double tanimoto(const std::vector<double>& one, const std::vector<double>& two)
 {
-  assert(one.size() == two.size());
-
-  double oneSqrSum = 0.0;
-  double twoSqrSum = 0.0;
-  double prodSum = 0.0;
-  
-  for(int i = 0; i < one.size(); i++)
-  {
-    prodSum += (one[i] * two[i]);
-    oneSqrSum += one[i] * one[i];
-    twoSqrSum += two[i] * two[i];
-  }
-
-  if((oneSqrSum + twoSqrSum - prodSum) == 0)
-    return 1.0;
-
-  return prodSum/(oneSqrSum + twoSqrSum - prodSum);
+	assert(one.size() == two.size());
+	
+	double oneSqrSum = 0.0;
+	double twoSqrSum = 0.0;
+	double prodSum = 0.0;
+	
+	for(int i = 0; i < one.size(); i++)
+	{
+		prodSum += (one[i] * two[i]);
+		oneSqrSum += one[i] * one[i];
+		twoSqrSum += two[i] * two[i];
+  	}
+	
+	if((oneSqrSum + twoSqrSum - prodSum) == 0)
+		return 1.0;
+	
+	return prodSum/(oneSqrSum + twoSqrSum - prodSum);
 }
 
 double manhattanDistance(const std::vector<double>& one, const std::vector<double>& two)
 {
-  assert(one.size() == two.size());
-
-  double distance = 0.0;
-  for(int i = 0; i < one.size(); i++)
-    distance += abs(one[i] - two[i]);
-
-  return distance;
+	assert(one.size() == two.size());
+	
+	double distance = 0.0;
+	
+	for(int i = 0; i < one.size(); i++)
+		distance += abs(one[i] - two[i]);
+		
+	return distance;
 }
 
 double chebychev(const std::vector<double>& one, const std::vector<double>& two)
 {
-  assert(one.size() == two.size());
-
-  double max = std::numeric_limits<double>::min();
-
-  for(int i = 0; i < one.size(); i++)
-    if(abs(one[i] - two[i]) > max)
-      max = abs(one[i] - two[i]);
-
-  return max;
+	assert(one.size() == two.size());
+	double max = std::numeric_limits<double>::min();
+	
+	for(int i = 0; i < one.size(); i++)
+		if(abs(one[i] - two[i]) > max)
+			max = abs(one[i] - two[i]);
+	
+	return max;
 }
 
 // NOT SURE ABOUT THIS DISTANCE MEASURE
 double slopeOneDistance(const std::vector<double>& one, const std::vector<double>& two)
 {
-  assert(one.size() == two.size());
-
-  double sumDiff = 0.0;
-  for(int i = 0; i < one.size(); i++)
-  {
-    sumDiff += (one[i] - two[i]);
-  }
-
-  return sumDiff;
+	assert(one.size() == two.size());
+	
+	double sumDiff = 0.0;
+	for(int i = 0; i < one.size(); i++)
+	{
+		sumDiff += (one[i] - two[i]);
+  	}
+	
+	return sumDiff;
 }
