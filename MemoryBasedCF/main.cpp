5394af76de758a892959b957038ee2862fbe0b1b
Ameya Vilankar <ameya.vilankar@gmail.com>
2013-06-11 17:25:53 -0400

    Added the main function to implement the CF Algorithm.

---
+++ MemoryBasedCF/main.cpp
@@ -0,0 +1,51 @@
+#include "main.h"
+
+// Using Declarations
+using std::cin;
+using std::cout;
+using std::endl;
+using std::sort;
+using std::string;
+using std::vector;
+using std::ifstream;
+using std::map;
+using std::ofstream;
+using std::sort;
+using std::pair;
+
+int main()
+{
+	// Load the dataset into the map
+	cout<<"Loading the Dataset...\n";
+	std::map<long, std::map<std::string, double> > userToMovie;
+	int errorval = loadDataSet("movies.dat", "rating.dat", userToMovie);
+	if(errorval != 0)
+		return errorval;
+
+	// USER-BASED CF
+	cout<<"Starting the User Based CF...\n";
+	cout<<"Getting the Recommended Items for each person...\n";
+	map<long, vector<pair<string, double> > > recommendedItemsUserBased = getRecommendationsUserBased(userToMovie);
+
+	cout<<"Getting the Top Similar Users for each Person...\n";
+	map<long, vector<pair<long, double> > > topSimilarUsers = calculateSimilarUsers(userToMovie);
+
+	// ITEM-BASED CF
+	cout<<"Starting the Item Based CF...\n";
+	cout<<"Transforming the map...";
+	map<string, map<long, double> >movieToUser = transformPrefs(userToMovie);
+	
+	cout<<"Calculating the top similar Movies for each movie...\n";
+	map<string, vector<pair<string, double> > > topSimilarMovies = calculateSimilarMovies(movieToUser);
+	
+	cout<<"Calculating the recommendations for each user...\n";
+	map<long, vector<pair<string, double> > > recommendedItemsItemBased = getRecommendationsItemBased(movieToUser);
+
+	cout<<"Saving to files...\n";
+	cout<<"Saving Recommendations UserBased...\n";
+	cout<<"Saving Top Similar Users...\n";
+	cout<<"Saving Top Similar Movies...\n";
+	cout<<"Saving Recommendations ItemBased...\n";
+
+	return 0;
+}
---
+++ MemoryBasedCF/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// main.h -v1.0
+// Author: Ameya Vilankar
+#include "utilityfunctions.h"
+#include "distances.h"
+#include "collaborativefiltering.h"
+
+#endif
