3b83bc98068e5754085a6bfeccc1b85977e3bb5d
Ameya Vilankar <ameya.vilankar@gmail.com>
2013-06-18 15:26:03 -0400

    Change the codes into modularized funtions.

--- Visualise/cosine/visualise_cosine.cpp
+++ Visualise/cosine/visualise_cosine.cpp
@@ -102,42 +102,11 @@ int getClusterCenters(const char* filename, vector<vector<double> >& clusterCent
     return 0;
 }
 
-int main()
+void calculateClusterCenters(map<int, vector<long> >& clusterToUserMap, map<long, vector<double> >& ratingMatrix, map<int, map<long, int> >& clusterUserDistances, map<int, vector<double> >& clusterCenters, map<int, long>& clusterCenterUser)
 {
-	// STEP 1: Read the clusters from the data.txt file and 
-	// Create a map from every user to its cluster number 
-	map<long, int> userToClusterMap;
-	map<int, vector<long> > clusterToUserMap;
-
-	int errorVal = getUserToClusterMap("data.txt", userToClusterMap, clusterToUserMap);
-	if(errorVal != 0)
-		return errorVal;
-	cout<<"No. of users: "<<userToClusterMap.size()<<endl;
-	cout<<"No. of Clusters:"<<clusterToUserMap.size()<<endl;
-	
-	/*
-	// STEP 2: Read the cluster centers from the "cluster.txt" file
-	vector<vector<double> > clusterCentersRDim;	
-	errorVal = getClusterCenters("cluster.txt", clusterCentersRDim);
-	if(errorVal != 0)
-		return errorVal;
-	cout<<"Cluster Dimensions: "<<clusterCentersRDim.size()<<", "<<clusterCentersRDim[0].size()<<endl;
-	*/
-
-	// STEP 3: Read the ratingMatrix to get the user vectors
-	map<long, vector<double> > ratingMatrix;
-	errorVal =  getRatingMatrix("ratings_with_id.txt", ratingMatrix);
-	if(errorVal != 0)
-		return errorVal;
-
-	cout<<"RatingMatrix Dimensions: "<<ratingMatrix.size()<<", "<<ratingMatrix[1].size()<<endl;
-
-	// Come up with the cluster centers
-	map<int, map<long, int> > clusterUserDistances;
-	map<int, vector<double> > clusterCenters;
-	map<int, long> clusterCenterUser;
-	
+	// Keep track of the number of users
 	int numberOfUsers = 0;
+
 	for(map<int, vector<long> >::iterator it = clusterToUserMap.begin(); it != clusterToUserMap.end(); it++)
 	{
 		//cout<<"Cluster No: "<<it->first<<", Number of users: "<<it->second.size()<<endl;
@@ -154,8 +123,7 @@ int main()
 		    
 		    // Store the sum
 		    for(int i = 0; i < currentCenter.size(); i++)
-			currentCenter[i] += temp[i];
-		    
+			currentCenter[i] += temp[i];  
 		}
 		
 		// Get the mean of all the clusters 
@@ -190,8 +158,10 @@ int main()
 	cout<<"Number of Clusters:"<<clusterCenters.size()<<endl;
 	cout<<"Number of Users: "<<numberOfUsers<<endl;
 
-	// STEP 4: Calculate the CANBERRA distance of every user to the all the clusters and
-	// find the cluster no. corresponding to the minimum distance
+}
+
+map<long, int> getCheckMap(const map<long, vector<double> >& ratingMatrix, map<int, vector<double> >& clusterCenters)
+{
 	map<long, int> checkMap;
 
 	for(map<long, vector<double> >::const_iterator rating_iter = ratingMatrix.begin(); rating_iter != ratingMatrix.end(); rating_iter++)
@@ -208,23 +178,173 @@ int main()
 
 	    for(map<int, vector<double> >::iterator cluster_it = clusterCenters.begin(); cluster_it != clusterCenters.end(); cluster_it++)
 	    {
-		//cout<<cluster_it->first<<" ";
+			//cout<<cluster_it->first<<" ";
 
-		// Calculate the canberra distance
-		currentDistance = exp(-cosineSimilarity(vector<double>(rating_iter->second.begin(), rating_iter->second.end()), vector<double>(cluster_it->second.begin(), cluster_it->second.end())));
-			
-		// TODO:: TODO:: check for multiple matches to minvalue
-		if(currentDistance < minValue)
-		{
-		    minValue = currentDistance;
-		    minIndex = cluster_it->first;
-		}
+			// Calculate the canberra distance
+			currentDistance = exp(-cosineSimilarity(vector<double>(rating_iter->second.begin(), rating_iter->second.end()), vector<double>(cluster_it->second.begin(), cluster_it->second.end())));
+				
+			// TODO:: TODO:: check for multiple matches to minvalue
+			if(currentDistance < minValue)
+			{
+			    minValue = currentDistance;
+			    minIndex = cluster_it->first;
+			}
+
 	    }
 
 	    // Set the cluster for the corresponding user
 	    checkMap[rating_iter->first] = minIndex;		
 	}
 
+	return checkMap;
+}
+
+map<int, map<int, int> > calculateClusterToClusterDistances(map<int, vector<double> >& clusterCenters)
+{
+	map<int, map<int, int> > clusterToClusterDistances;
+	int count = 0;
+	for(map<int, vector<double> >::const_iterator it = clusterCenters.begin(); it != clusterCenters.end(); it++)
+	{
+		map<int, vector<double> >::const_iterator second_it = it;
+		second_it++;
+		while(second_it != clusterCenters.end())
+		{
+			count++;
+			//cout<<"Calculating: "<<it->first<<" "<<second_it->first<<endl<<" "<<count<<endl;
+			int distance = (int)(exp(-cosineSimilarity(it->second, second_it->second)) * 100);
+			clusterToClusterDistances[it->first][second_it->first] = distance;
+			second_it++;
+		}
+	}
+	
+	cout<<"ClusterDistanceMatrixDimenstions: "<<clusterToClusterDistances.size()<<" , "<<clusterToClusterDistances.begin()->second.size()<<endl;
+	
+	return clusterToClusterDistances;
+}
+
+void saveCheckMap(map<long, int>& userToClusterMap, map<long, int>& checkMap)
+{
+	int correct = 0;
+
+	// Save the Checkcluster to file TODO: Rewrite as a function
+	ofstream outfile;
+	outfile.open("CheckCluster.txt");
+
+	// Compare the distances and Check if right.
+	outfile<<"User:\t Cluster1\t Cluster2\n";
+	
+	for(map<long, int>::const_iterator it = userToClusterMap.begin(); it != userToClusterMap.end(); it++)
+	{
+	    if(it->second == checkMap[it->first])
+		correct++;
+	    outfile<<it->first<<"\t"<<it->second<<"\t"<<checkMap[it->first]<<endl;
+	}
+	
+	cout<<"Correct:"<<correct<<"/"<<userToClusterMap.size()<<"\n";
+	outfile.close();
+}
+
+void saveJSONFiles(map<int, vector<long> >& clusterToUserMap, map<int, map<int, int> >& clusterToClusterDistances, map<int, map<long, int> >& clusterUserDistances)
+{
+	ofstream outfile1, outfile2;
+	outfile1.open("nodes.json");
+	outfile2.open("links.json");
+		
+	// save to file the first line of the file
+	outfile1<<"{\n\"nodes\":[\n";
+	outfile2<<"{\n\"links\":[\n";
+	
+	//print the first entry of the file
+	//outfile2<<"{\"source\":"
+	//outfile1<<"{\"name\":\""<<clusterToUserMap.begin()->second[0]<<"\",\"group\":"<<clusterToUserMap.begin()->first<<"}";
+	
+	int lineCount = 0;
+	int clusterLine = 0;
+	map<int, int> clusterLineMap;
+	int linkCount = 0;
+
+	for(map<int, vector<long> >::const_iterator it = clusterToUserMap.begin(); it != clusterToUserMap.end(); it++)
+	{
+		// Enter the cluster to the file
+		outfile1<<"{\"name\":\"c"<<it->first<<"\",\"group\":"<<it->first<<"},\n";
+		clusterLineMap[it->first] = lineCount++;
+	
+		for(int i = 0; i < it->second.size(); i++)
+		{
+			//outfile1<<"{\"name\":\""<<it->second[i]<<"\",\"group\":"<<it->first<<"},\n";
+			//outfile2<<"{\"source\":"<<clusterLineMap[it->first]<<",\"target\":"<<lineCount<<",\"value\":"<<clusterUserDistances[it->first][it->second[i]]<<"},\n";
+			//lineCount++;
+			linkCount++;
+		}
+
+		//cout<<"Cluster No: "<<it->first<<" ClusterLine: "<<clusterLineMap[it->first]<<" No. of Users: "<<it->second.size()<<" LineCount Now: "<<lineCount<<endl;
+	}
+
+	cout<<"LineCount Now: "<<lineCount<<endl;
+	cout<<"LinkCount: "<<linkCount<<endl;
+	
+	// Add all the lines from one cluster to another
+	for(map<int, map<int, int> >::const_iterator it = clusterToClusterDistances.begin(); it != clusterToClusterDistances.end(); it++)
+	{
+		for(map<int, int>::const_iterator second_it = it->second.begin(); second_it != it->second.end(); second_it++)
+		{
+			linkCount++;
+			//cout<<"Adding Links from: Cluster "<<it->first<<" to Cluster "<<second_it->first<<endl;
+			outfile2<<"{\"source\":"<<clusterLineMap[it->first]<<",\"target\":"<<clusterLineMap[second_it->first]<<",\"value\":"<<clusterToClusterDistances[it->first][second_it->first]<<"},\n";
+		}
+	}
+	
+	cout<<"LinkCount: "<<linkCount<<endl;
+	
+	outfile1<<"\n]\n}";
+	outfile2<<"\n]\n}";
+	
+	// Close the two writing files
+	outfile1.close();
+	outfile2.close();
+}
+
+
+int main()
+{
+	// STEP 1: Read the clusters from the data.txt file and 
+	// Create a map from every user to its cluster number 
+	map<long, int> userToClusterMap;
+	map<int, vector<long> > clusterToUserMap;
+
+	int errorVal = getUserToClusterMap("data.txt", userToClusterMap, clusterToUserMap);
+	if(errorVal != 0)
+		return errorVal;
+	cout<<"No. of users: "<<userToClusterMap.size()<<endl;
+	cout<<"No. of Clusters:"<<clusterToUserMap.size()<<endl;
+	
+	/*
+	// STEP 2: Read the cluster centers from the "cluster.txt" file
+	vector<vector<double> > clusterCentersRDim;	
+	errorVal = getClusterCenters("cluster.txt", clusterCentersRDim);
+	if(errorVal != 0)
+		return errorVal;
+	cout<<"Cluster Dimensions: "<<clusterCentersRDim.size()<<", "<<clusterCentersRDim[0].size()<<endl;
+	*/
+
+	// STEP 3: Read the ratingMatrix to get the user vectors
+	map<long, vector<double> > ratingMatrix;
+	errorVal =  getRatingMatrix("ratings_with_id.txt", ratingMatrix);
+	if(errorVal != 0)
+		return errorVal;
+
+	cout<<"RatingMatrix Dimensions: "<<ratingMatrix.size()<<", "<<ratingMatrix[1].size()<<endl;
+
+	// Come up with the cluster centers, distances of users to their cluster centers, and the user who is at the center of each cluster
+	map<int, map<long, int> > clusterUserDistances;
+	map<int, vector<double> > clusterCenters;
+	map<int, long> clusterCenterUser;
+	calculateClusterCenters(clusterToUserMap, ratingMatrix, clusterUserDistances, clusterCenters, clusterCenterUser);
+	
+	// STEP 4: Calculate the CANBERRA distance of every user to the all the clusters and
+	// find the cluster no. corresponding to the minimum distance
+	map<long, int> checkMap = getCheckMap(ratingMatrix, clusterCenters);
+	
 	
 	/*
 	vector<vector<double> > ratingArray;
@@ -272,100 +392,10 @@ int main()
 	*/
 
 	// Develop the distance matrix from one cluster to another
-	map<int, map<int, int> > clusterToClusterDistances;
-	int count = 0;
-	for(map<int, vector<double> >::const_iterator it = clusterCenters.begin(); it != clusterCenters.end(); it++)
-	{
-		map<int, vector<double> >::const_iterator second_it = it;
-		second_it++;
-		while(second_it != clusterCenters.end())
-		{
-			count++;
-			//cout<<"Calculating: "<<it->first<<" "<<second_it->first<<endl<<" "<<count<<endl;
-			int distance = (int)(exp(-cosineSimilarity(it->second, second_it->second)) * 100);
-			clusterToClusterDistances[it->first][second_it->first] = distance;
-			second_it++;
-		}
-	}
-	
-	cout<<"ClusterDistanceMatrixDimenstions: "<<clusterToClusterDistances.size()<<" , "<<clusterToClusterDistances.begin()->second.size()<<endl;
-	int correct = 0;
-	{
-		// Save the Checkcluster to file TODO: Rewrite as a function
-		ofstream outfile;
-		outfile.open("CheckCluster.txt");
+	map<int, map<int, int> > clusterToClusterDistances = calculateClusterToClusterDistances(clusterCenters);
 
-		// Compare the distances and Check if right.
-		outfile<<"User:\t Cluster1\t Cluster2\n";
-   		for(map<long, int>::const_iterator it = userToClusterMap.begin(); it != userToClusterMap.end(); it++)
-		{
-		    if(it->second == checkMap[it->first])
-			correct++;
-		    outfile<<it->first<<"\t"<<it->second<<"\t"<<checkMap[it->first]<<endl;
-		}
-		
-		cout<<"Correct:"<<correct<<"/"<<userToClusterMap.size()<<"\n";
-		outfile.close();
-	}
-
-	{
-		ofstream outfile1, outfile2;
-		outfile1.open("nodes.json");
-		outfile2.open("links.json");
-			
-		// save to file the first line of the file
-		outfile1<<"{\n\"nodes\":[\n";
-		outfile2<<"{\n\"links\":[\n";
-		
-		//print the first entry of the file
-		//outfile2<<"{\"source\":"
-		//outfile1<<"{\"name\":\""<<clusterToUserMap.begin()->second[0]<<"\",\"group\":"<<clusterToUserMap.begin()->first<<"}";
-		
-		int lineCount = 0;
-		int clusterLine = 0;
-		map<int, int> clusterLineMap;
-		int linkCount = 0;
-
-		for(map<int, vector<long> >::const_iterator it = clusterToUserMap.begin(); it != clusterToUserMap.end(); it++)
-		{
-			// Enter the cluster to the file
-			outfile1<<"{\"name\":\"c"<<it->first<<"\",\"group\":"<<it->first<<"},\n";
-			clusterLineMap[it->first] = lineCount++;
-		
-			for(int i = 0; i < it->second.size(); i++)
-			{
-				//outfile1<<"{\"name\":\""<<it->second[i]<<"\",\"group\":"<<it->first<<"},\n";
-				//outfile2<<"{\"source\":"<<clusterLineMap[it->first]<<",\"target\":"<<lineCount<<",\"value\":"<<clusterUserDistances[it->first][it->second[i]]<<"},\n";
-				//lineCount++;
-				linkCount++;
-			}
-
-			//cout<<"Cluster No: "<<it->first<<" ClusterLine: "<<clusterLineMap[it->first]<<" No. of Users: "<<it->second.size()<<" LineCount Now: "<<lineCount<<endl;
-		}
-
-		cout<<"LineCount Now: "<<lineCount<<endl;
-		cout<<"LinkCount: "<<linkCount<<endl;
-		
-		// Add all the lines from one cluster to another
-		for(map<int, map<int, int> >::const_iterator it = clusterToClusterDistances.begin(); it != clusterToClusterDistances.end(); it++)
-		{
-			for(map<int, int>::const_iterator second_it = it->second.begin(); second_it != it->second.end(); second_it++)
-			{
-				linkCount++;
-				//cout<<"Adding Links from: Cluster "<<it->first<<" to Cluster "<<second_it->first<<endl;
-				outfile2<<"{\"source\":"<<clusterLineMap[it->first]<<",\"target\":"<<clusterLineMap[second_it->first]<<",\"value\":"<<clusterToClusterDistances[it->first][second_it->first]<<"},\n";
-			}
-		}
-		
-		cout<<"LinkCount: "<<linkCount<<endl;
-		
-		outfile1<<"\n]\n}";
-		outfile2<<"\n]\n}";
-		
-		// Close the two writing files
-		outfile1.close();
-		outfile2.close();
-	}
+	// Save the checkMap to the file
+	saveJSONFiles(clusterToUserMap, clusterToClusterDistances, clusterUserDistances);
     
 	return 0;
 
